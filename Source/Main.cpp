bf6b24b6e5495b996b9835866728cd6c1f654587
Max Burnell <spacer.contact@gmail.com>
2025-02-14 14:09:33 +0000

    For_Loop_Buffers
    Loops over each buffer, if within sample length, adds sample1 & 2.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -22,28 +22,28 @@ int main (int argc, char* argv[])
         //buffer1
     juce::File inputFile1("E:\\JUCE\\JUCE Projects\\Tone.wav");
     FileInputStream* inputStream1 = new FileInputStream(inputFile1);
-    AudioFormatReader* reader = format.createReaderFor(inputStream1, true);
-    int numSamples1 = static_cast<int>(reader->lengthInSamples);
+    AudioFormatReader* reader1 = format.createReaderFor(inputStream1, true);
+    int numSamples1 = static_cast<int>(reader1->lengthInSamples);
     AudioBuffer<float> buffer1(1, numSamples1);
-    reader->read(&buffer1, 0, numSamples1, 0, true, false);
+    reader1->read(&buffer1, 0, numSamples1, 0, true, false);
     std::cout << "done reading" << std::endl;
-    delete reader;
+    delete reader1;
 
     // Read file2
         //buffer2
     juce::File inputFile2("E:\\JUCE\\JUCE Projects\\Tone2.wav");
     FileInputStream* inputStream2 = new FileInputStream(inputFile2);
-    AudioFormatReader* reader = format.createReaderFor(inputStream2, true);
-    int numSamples2 = static_cast<int>(reader->lengthInSamples);
+    AudioFormatReader* reader2 = format.createReaderFor(inputStream2, true);
+    int numSamples2 = static_cast<int>(reader2->lengthInSamples);
     AudioBuffer<float> buffer2(1, numSamples2);
-    reader->read(&buffer2, 0, numSamples2, 0, true, false);
+    reader2->read(&buffer2, 0, numSamples2, 0, true, false);
     std::cout << "done reading" << std::endl;
-    delete reader;
+    delete reader2;
 
 
     //apply gain
     buffer1.applyGain(0.5f);
-    buffer2.applyGain(0.5f);
+    buffer2.applyGain(0.2f);
 
     //buffer for longest input
         //output buffer for combined samples
@@ -54,6 +54,30 @@ int main (int argc, char* argv[])
     const float* readPtr1 = buffer1.getReadPointer(0);
     const float* readPtr2 = buffer2.getReadPointer(0);
     float* writePtr = outputBuffer.getWritePointer(0);
+
+    //mix buffers
+        //loop for length of longest sample (outputSamples)
+    for (int i = 0; i < outputSamples; i++) {
+        
+        float sample1, sample2;
+
+        if (i < numSamples1) {
+            sample1 = readPtr1[i];
+        }
+        else {
+            sample1 = 0.0f;
+        }
+
+        if (i < numSamples2) {
+            sample2 = readPtr2[i];
+        }
+        else {
+            sample2 = 0.0f;
+        }
+
+        writePtr[i] = sample1 + sample2;
+
+    }
 
     // Write file
     juce::File output("E:\\JUCE\\JUCE Projects\\ToneMix.wav");
